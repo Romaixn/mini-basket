31b11668731e071ccc6ce9132a8a9b5279fcd565
Romain <romain.herault@les-tilleuls.coop>
2023-06-28 16:44:29 +0200

    fix: change color

--- src/Components/Table.tsx
+++ src/Components/Table.tsx
@@ -10,8 +10,8 @@ type GLTFResult = GLTF & {
     Controls: THREE.Mesh;
     Control_A: THREE.Mesh;
     Control_B: THREE.Mesh;
-    Propulsor_A: THREE.Mesh;
-    Propulsor_B: THREE.Mesh;
+    Thruster_A: THREE.Mesh;
+    Thruster_B: THREE.Mesh;
   };
   materials: {
     Wood: THREE.MeshStandardMaterial;
@@ -42,44 +42,44 @@ export default function Table(props: JSX.IntrinsicElements["group"]) {
                 <MeshTransmissionMaterial anisotropy={0.1} chromaticAberration={0.04} distortionScale={0} temporalDistortion={0} />
         </mesh>
         <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Controls.geometry}
-        material={materials.Wood}
-        position={[4.183, 0.092, -0.003]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Control_A.geometry}
-        material={materials.Red}
-        position={[4.311, -0.117, 0.688]}
-        rotation={[0, 0, -0.391]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Control_B.geometry}
-        material={materials.Green}
-        position={[4.311, -0.119, -0.811]}
-        rotation={[0, 0, -0.39]}
-      />
-            <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Propulsor_B.geometry}
-        material={materials.Black}
-        position={[2.259, -0.11, -0.764]}
-        scale={0.573}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Propulsor_A.geometry}
-        material={materials.Black}
-        position={[2.259, -0.11, 0.765]}
-        scale={0.573}
-      />
+            castShadow
+            receiveShadow
+            geometry={nodes.Controls.geometry}
+            material={materials.Wood}
+            position={[4.183, 0.092, -0.003]}
+        />
+        <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Control_A.geometry}
+            material={materials.Red}
+            position={[4.311, -0.117, 0.688]}
+            rotation={[0, 0, -0.391]}
+        />
+        <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Control_B.geometry}
+            material={materials.Green}
+            position={[4.311, -0.119, -0.811]}
+            rotation={[0, 0, -0.39]}
+        />
+        <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Thruster_A.geometry}
+            material={materials.Black}
+            position={[2.259, -0.11, 0.765]}
+            scale={0.573}
+        />
+        <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Thruster_B.geometry}
+            material={materials.Black}
+            position={[2.259, -0.11, -0.764]}
+            scale={0.573}
+        />
     </group>
   );
 }
