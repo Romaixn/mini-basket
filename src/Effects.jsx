ef46a356742b065cfe059303884ea2e8a80b512e
Romain <romain.herault@les-tilleuls.coop>
2023-08-08 16:21:49 +0200

    feat: improve performance

--- src/Effects.jsx
+++ src/Effects.jsx
@@ -1,8 +1,7 @@
-import { Bloom, DepthOfField, EffectComposer } from "@react-three/postprocessing";
+import { DepthOfField, EffectComposer } from "@react-three/postprocessing";
 
 export default function Effects() {
     return <EffectComposer>
-        <Bloom luminanceSmoothing={0.06} />
         <DepthOfField bokehScale={10} />
     </EffectComposer>
 }
