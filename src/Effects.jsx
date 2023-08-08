6abbb10900d4609517c0066fd127be63fd1a2ca3
Romain <romain.herault@les-tilleuls.coop>
2023-08-08 16:09:30 +0200

    feat: remove vignette effect

--- src/Effects.jsx
+++ src/Effects.jsx
@@ -1,9 +1,8 @@
-import { Bloom, DepthOfField, EffectComposer, SSAO, ToneMapping, Vignette } from "@react-three/postprocessing";
+import { Bloom, DepthOfField, EffectComposer } from "@react-three/postprocessing";
 
 export default function Effects() {
     return <EffectComposer>
         <Bloom luminanceSmoothing={0.06} />
         <DepthOfField bokehScale={10} />
-        <Vignette darkness={0.4} />
     </EffectComposer>
 }
